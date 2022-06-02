fa4080fae1ed1f7a5434a31fda136b81a881b916
mariana-santos <marianasfernandessousa@gmail.com>
2022-06-01 22:04:14 -0300

    :twisted_rightwards_arrows: merge de novo

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -25,15 +25,9 @@ export default class About extends Component {
                     <div className="container-about">
                         <h1 style={{fontSize: '3rem', marginTop: '2rem'}}>Como a Nutri Tips nasceu? </h1>
 
-<<<<<<< HEAD
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl quam, vulputate ut interdum ut, vehicula nec ante. Vivamus non nisi ornare, vestibulum elit at, ullamcorper tellus. Aliquam posuere iaculis scelerisque. Mauris lacus lacus, pretium eu arcu ornare, scelerisque tempus lectus. </p>
 
                         <p>Vestibulum in nunc aliquam, mollis urna non, condimentum dolor. Vivamus iaculis enim dolor, ut convallis orci interdum convallis. Suspendisse dignissim ipsum justo, ut sollicitudin erat vestibulum eget. Nam mollis luctus vehicula. Mauris mollis nunc eget lectus varius, a finibus erat consectetur. </p>
-=======
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl quam, vulputate ut interdum ut, vehicula nec ante. Vivamus non nisi ornare, vestibulum elit at, ullamcorper tellus. Aliquam posuere iaculis scelerisque. Mauris lacus lacus, pretium eu arcu ornare, scelerisque tempus lectus. Pellentesque ligula turpis, scelerisque quis metus eu, tempor malesuada mauris. </p>
-
-                        <p>Vestibulum in nunc aliquam, mollis urna non, condimentum dolor. Vivamus iaculis enim dolor, ut convallis orci interdum convallis. Suspendisse dignissim ipsum justo, ut sollicitudin erat vestibulum eget.</p>
->>>>>>> 55934cf0f20f69a25d8493d2ce2e9e926992641c
                     </div>
 
                     <div className="container-redes">
